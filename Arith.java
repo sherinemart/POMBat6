ced0c12fa67deb97dcf28fabb88623de0d4e4fb6
sherinemart <91425378+sherinemart@users.noreply.github.com>
2023-10-04 13:19:34 +0530

    First commit

---
+++ Arith.java
@@ -0,0 +1,23 @@
+class Arith
+{
+ public static void main(String args[])
+{
+ System.out.println("Hello world");
+ int a,b,c,d;
+ a=10;
+ b=5;
+ float f=a;
+ float r=4.56f;
+ int y=(int)r;
+ b=b+1;// b++;
+ System.out.println(b);
+ System.out.println("The float value: "+f);
+ System.out.println("Int to float: "+y);
+ System.out.println(a+b);
+ System.out.println(a-b);
+ System.out.println(a*b);
+ System.out.println(a/b);
+ System.out.println(a%b);
+ 
+}
+}
